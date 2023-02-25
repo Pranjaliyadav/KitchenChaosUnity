f3b9dc5c336d06760338740b88c3b0c4daac8556
PranjaliNu10 <yadavpranjali1223@gmail.com>
2023-02-25 21:14:55 +0530

    anim done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    private bool isWalking;
     
     private void Update() {
         Vector3 moveDir =new Vector3(0,0,0);
@@ -24,9 +25,15 @@ public class PlayerController : MonoBehaviour
         float moveAcceleration = 8f;
         transform.position += moveDir*Time.deltaTime*moveAcceleration;
 
+        isWalking = moveDir != Vector3.zero; //if moveDir is diff than 0,0,0
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
 
 
+    }
+
+    public bool IsWalking(){
+        return isWalking;
     }
 }
