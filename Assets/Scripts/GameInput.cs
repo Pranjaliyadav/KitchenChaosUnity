b6db56cfc23f55b51da66a6ac34834a59d3a5732
Pranjali Yadav <yadavpranjali1223@gmail.com>
2023-02-28 15:20:11 +0530

    error resolved

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    private PlayerInputActions playerInputActions;
+    //private PlayerInputActions playerInputActions;
 
-    private void Awake() {
-        playerInputActions = new PlayerInputActions();
-        playerInputActions.Player.Enable(); //this is how you enable Input Actions 
-    }
-   public Vector2 GetMovementVectorNormalized(){
+    //private void Awake()
+    //{
+    //    playerInputActions = new PlayerInputActions();
+    //    playerInputActions.Player.Enable(); //this is how you enable Input Actions 
+    //}
+    public Vector3 GetMovementVectorNormalized()
+    {
 
+        Vector3 moveDir = new Vector3(0, 0, 0);
 
-        Vector2 moveDir =  playerInputActions.Player.Move.ReadValue<Vector3>();
+        //playerInputActions.Player.Move.ReadValue<Vector3>();
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir.z += +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir.z += -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDir.x += -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir.x += +1f;
+        }
         moveDir = moveDir.normalized; //this way if you press two keys at once it'll move same amount in both direction diagnonally summing to 1
-        
-         return moveDir;
-
-   }
+        return moveDir;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,39 @@ public class PlayerController : MonoBehaviour
 {   
     private bool isWalking;
 
-    [SerializeField] private GameInput gameInput;
-    
-    
+    //[SerializeField] private GameInput gameInput;
+    float moveAcceleration = 7f;
+    float rotateSpeed = 10f;
     private void Update() {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
+        Vector3 moveDir = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir.z += +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir.z += -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDir.x += -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir.x += +1f;
+        }
 
-        float moveAcceleration = 8f;
-        transform.position += moveDir*Time.deltaTime*moveAcceleration;
+        moveDir = moveDir.normalized; //this way if you press two keys at once it'll move same amount in both direction diagnonally summing to 1
+        
+        transform.position += moveDir * Time.deltaTime * moveAcceleration;
 
         isWalking = moveDir != Vector3.zero; //if moveDir is diff than 0,0,0
 
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
 
+        if (moveDir != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        }
 
     }
 
